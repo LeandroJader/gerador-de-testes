6e275744bacb84fd49f7c3feacd5852a54d40555
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-07-17 16:07:32 -0300

    Adicionado configuração de logs com Serilog

---
+++ GeradorDeTestes.WebApp/DependencyInjection/SerilogConfig.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace GeradorDeTestes.WebApp.DependencyInjection;
+
+public static class SerilogConfig
+{
+    public static void AddSerilogConfig(this IServiceCollection services, ILoggingBuilder logging)
+    {
+        var caminhoAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        var caminhoArquivoLogs = Path.Combine(caminhoAppData, "ControleDeBar", "error.log");
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File(caminhoArquivoLogs, LogEventLevel.Error)
+            .CreateLogger();
+
+        logging.ClearProviders();
+
+        services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+    }
+}
